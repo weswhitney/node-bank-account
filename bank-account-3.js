8210b9a0a886ec8b9caf89116151aa52ac63bc21
weswhitney <wesleywhitney@gmail.com>
2016-05-28 16:53:30 -0600

    bank account tutorial

--- bank-account-3.js
+++ bank-account-3.js
@@ -7,22 +7,28 @@ function createAccount(account) {
 
 function getAccount(username) {
     var matchedAccount;
-    //for loop or while loop
-    accounts.forEach(function(account) {
-        if (username === account.username) {
-            matchedAccount = account;
+    var i;
+    for (i = 0;i < accounts.length;i++) {
+        if (username === accounts[i].username) {
+            matchedAccount = accounts[i];
         }
-    });
+    }
     return matchedAccount;
 }
 
 function deposit(account, amount) {
     // amount only accepts number, use typeof
+    if (typeof amount !== 'number') {
+        return console.log("amount must be a number");
+    }
     account.balance += amount;
 }
 
 function withdraw(account, amount) {
     // amount only accepts number
+    if (typeof amount !== 'number') {
+        return console.log("amount must be a number");
+    }
     account.balance -= amount;
 }
 
@@ -30,6 +36,31 @@ function getBalance(account) {
     return account.balance;
 }
 
-// createBalanceGetter(account)
-//  return function ()
-//      account.balance (return it)
+function createBalanceGetter(account) {
+   return  function() {
+        return account.balance;
+   };
+}
+
+var wesAccount = createAccount({
+    balance: 400,
+    username: 'weswhitney'
+});
+
+var guysAccount = createAccount({
+    balance: 1200000,
+    username: 'guyrichy'
+});
+
+deposit(wesAccount, 230);
+
+withdraw(wesAccount, 100);
+
+var existingAccount = getAccount('weswhitney');
+
+var balanceGetter = createBalanceGetter(wesAccount);
+var guysBalanceGetter = createBalanceGetter(guysAccount);
+
+console.log(balanceGetter());
+console.log(guysBalanceGetter(guysAccount));
+
